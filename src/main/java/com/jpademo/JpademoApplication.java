04a34022c717832a9ba22cd93b96aee4a263a4c8
Sonali <sonalisinghblore@gmail.com>
2024-05-18 17:00:41 +0530

    Added my project

---
+++ src/main/java/com/jpademo/JpademoApplication.java
@@ -0,0 +1,14 @@
+package com.jpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpademoApplication.class, args);
+		System.out.println("Oracle database successfully connected.");
+	}
+
+}
